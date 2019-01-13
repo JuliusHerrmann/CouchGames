e04611e2883f6d56f6c36e55955fa009ac8dddb8
Christian <faberle42@googlemail.com>
2019-01-13 13:14:46 +0100

    added more boilerplate code to multiplayer

---
+++ Assets/Server.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class MultiplayerServer
+{
+   public void sendToHost(ISerializable data)
+    {
+        return;
+    }
+
+    public void sendToAll(ISerializable data)
+    {
+        return;
+    }
+
+
+
+}
